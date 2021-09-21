7a21c848a2d4993176e5f4d9e8fc023f32725c5e
outloop <72603205+outloops@users.noreply.github.com>
2021-09-21 10:24:43 +0200

    Update Misc.cpp

--- Misc.cpp
+++ Misc.cpp
@@ -7,7 +7,7 @@ void Misc::BunnyHop()
 		if (LocalPlayer != NULL) // check
 		{
 			int flag = *(BYTE*)(LocalPlayer + m_fFlags);
-			if (GetAsyncKeyState(VK_SPACE) && flag & (1 << 0))
+			if (GetAsyncKeyState(VK_SPACE) && flag & FL_ONGROUND)
 			{
 				*(DWORD*)(ClientBase + dwForceJump) = 6;
 			}
